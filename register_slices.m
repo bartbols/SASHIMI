cfa025a6756813c7a663c80ac2c31c66e81ac10c
Bart Bolsterlee <b.bolsterlee@neura.edu.au>
2020-04-16 15:05:48 +1000

    update to 2019b (stand alond version now needs runtime 9.7). Added menu options. Optimised registration (only parts of images that contain objects are registered).

--- register_slices.m
+++ register_slices.m
@@ -22,6 +22,8 @@ addParameter(p,'PyramidLevels',3)
 addParameter(p,'DisplayWaitbar',false)
 addParameter(p,'spacing',[1 1])
 addParameter(p,'method','linear')
+addParameter(p,'crop',false)
+addParameter(p,'padding',5,@isscalar)
 parse(p,img1,img2,varargin{:});
 
 % label1 = p.Results.label1;
@@ -30,6 +32,8 @@ N       = p.Results.N; % number of iterations
 afs     = p.Results.AccumulatedFieldSmoothing;
 pl      = p.Results.PyramidLevels;
 dw      = p.Results.DisplayWaitbar;
+crop    = p.Results.crop;
+padding = p.Results.padding;
 label1  = p.Results.label1; % label defined on the moving image
 points  = p.Results.points; % points in fixed image coordinates
 spacing = p.Results.spacing; % pixel spacing in x and y direction
@@ -46,10 +50,38 @@ points_tf = [];
 %                dimensions.
 % img1_reg     : warped moving image (should match the fixed image)
 
-[D,img1_reg] = imregdemons(img1,img2,N,...
+imdim = size(img2);
+xvec = (0 : imdim(2)-1) * spacing(2);
+yvec = (0 : imdim(1)-1) * spacing(1);
+
+if crop == true && ~isempty(points)
+    % Register only regions in which points are
+    if iscell(points)
+        minx = min(cellfun(@(x) min(x(:,1)),points));
+        maxx = max(cellfun(@(x) max(x(:,1)),points));
+        miny = min(cellfun(@(x) min(x(:,2)),points));
+        maxy = max(cellfun(@(x) max(x(:,2)),points));
+    else
+        minx = min(points(:,1));
+        maxx = max(points(:,1));
+        miny = min(points(:,2));
+        maxy = max(points(:,2));
+    end
+    sel_x = xvec > (minx - padding) & xvec < (maxx + padding);
+    sel_y = yvec > (miny - padding) & yvec < (maxy + padding);
+else
+    sel_x = true(size(xvec));
+    sel_y = true(size(yvec));
+
+end
+img1_reg = NaN(size(img2));
+D        = NaN([size(img2),2]);
+[D2,I_reg] = imregdemons(img1(sel_y,sel_x),img2(sel_y,sel_x),N,...
     'AccumulatedFieldSmoothing',afs,...
     'PyramidLevels',pl,...
     'DisplayWaitbar',dw);
+img1_reg(sel_y,sel_x,:) = I_reg;
+D(sel_y,sel_x,:) = D2;
 
 if ~isempty(label1)
     % Estimate the transformed label. label1_tf is label 1 transformed to
@@ -58,9 +90,6 @@ if ~isempty(label1)
 end
 
 % Transform the points
-imdim = size(img2);
-xvec = (0 : imdim(2)-1) * spacing(1);
-yvec = (0 : imdim(1)-1) * spacing(2);
 if ~isempty(points)
     if iscell(points)
         % Cell array with multiple point sets is provided. Transform all 
@@ -80,9 +109,13 @@ end
 % Sub-functions
     function pnew = get_new_location(pts)        
             d = zeros(size(pts));
-            d(:,1) = interp2(xvec,yvec,D(:,:,1),pts(:,1),pts(:,2),method);
-            d(:,2) = interp2(xvec,yvec,D(:,:,2),pts(:,1),pts(:,2),method);
-            pnew = pts + d.*spacing;        
+            d(:,1) = interp2(xvec,yvec,D(:,:,1),pts(:,1),pts(:,2),method); % x displacement in pixel coordinates
+            d(:,2) = interp2(xvec,yvec,D(:,:,2),pts(:,1),pts(:,2),method); % y displacement in pixel coordinates
+            % Update the new position vectors. The displacement field
+            % preditect by imregdemons is in pixel coordinates, so it needs
+            % to be converted back to image coordinates (flipped x/y axis,
+            % and multiplied by pixel size).
+            pnew = pts + d.*spacing([2 1]);        
     end
 %%
 if nargout > 0
--- standalone/readme.txt
+++ standalone/readme.txt
@@ -1,4 +1,4 @@
-SASHIMI_v1_0 Executable
+SASHIMI_v1_0b Executable
 
 1. Prerequisites for Deployment 
 
@@ -24,7 +24,7 @@ in the MathWorks Documentation Center.
 
 Files to Package for Standalone 
 ================================
--SASHIMI_v1_0.exe
+-SASHIMI_v1_0b.exe
 -MCRInstaller.exe 
     Note: if end users are unable to download the MATLAB Runtime using the
     instructions in the previous section, include it when building your 
