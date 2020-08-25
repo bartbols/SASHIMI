1a5d776731d373f78040872e3e002d0a51d84684
Bart <b.bolsterlee@neura.edu.au>
2020-08-25 19:15:38 +1000

    added old versions (may not work properly)

---
+++ old_versions/register_slices.m
@@ -0,0 +1,136 @@
+function varargout = register_slices(img1,img2,varargin)
+%REGISTER_SLICES Registers two 2D images and then applies the estimated
+%transformation field to transform the points of image 1 to image 2. This
+%function uses Matlab's built-in demons registration (see doc imregdemons
+%for details). The first input is the moving image and the second input
+%the fixed image.
+%
+% Bart Bolsterlee, Neuroscience Research Australia
+% March 2020
+%
+% Edits:
+% 24/4, BB: removed option to transform label field.
+% 24/4, BB: shifted coordinate system by half a pixel so that xvec/yvec
+% indicate the center of a pixel (was edge of the pixel)
+
+%% Parse inputs
+p = inputParser;
+addRequired(p,'img1')
+addRequired(p,'img2')
+% addRequired(p,'label1')
+% addParameter(p,'label1',[])
+addParameter(p,'points',[])
+
+% Registration settings
+addParameter(p,'N',100,@isscalar)
+addParameter(p,'AccumulatedFieldSmoothing',1.5)
+addParameter(p,'PyramidLevels',3)
+addParameter(p,'DisplayWaitbar',false)
+addParameter(p,'spacing',[1 1])
+addParameter(p,'method','linear')
+addParameter(p,'crop',false)
+addParameter(p,'padding',5,@isscalar)
+parse(p,img1,img2,varargin{:});
+
+% label1 = p.Results.label1;
+N       = p.Results.N; % number of iterations
+% demons settings. See 'doc imregdemons' for details.
+afs     = p.Results.AccumulatedFieldSmoothing;
+pl      = p.Results.PyramidLevels;
+dw      = p.Results.DisplayWaitbar;
+crop    = p.Results.crop;
+padding = p.Results.padding;
+% label1  = p.Results.label1; % label defined on the moving image
+points  = p.Results.points; % points in fixed image coordinates
+spacing = p.Results.spacing; % pixel spacing in x and y direction
+method  = p.Results.method; % interpolation method
+
+% label1_tf = [];
+points_tf = [];
+%% Register images
+
+% first input  : moving image
+% second input : fixed image (reference)
+% D            : displacement vectors from the fixed image grid to a
+%                corresponding location in the moving image in image pixel
+%                dimensions.
+% img1_reg     : warped moving image (should match the fixed image)
+
+% Build up vector of x and y location of pixel centres.
+imdim = size(img2);
+xvec = (0.5 : imdim(2)-0.5) * spacing(2);
+yvec = (0.5 : imdim(1)-0.5) * spacing(1);
+
+if crop == true && ~isempty(points)
+    % Register only regions in which points are provided.
+    if iscell(points)
+        minx = min(cellfun(@(x) min(x(:,1)),points));
+        maxx = max(cellfun(@(x) max(x(:,1)),points));
+        miny = min(cellfun(@(x) min(x(:,2)),points));
+        maxy = max(cellfun(@(x) max(x(:,2)),points));
+    else
+        minx = min(points(:,1));
+        maxx = max(points(:,1));
+        miny = min(points(:,2));
+        maxy = max(points(:,2));
+    end
+    sel_x = xvec > (minx - padding) & xvec < (maxx + padding);
+    sel_y = yvec > (miny - padding) & yvec < (maxy + padding);
+else
+    sel_x = true(size(xvec));
+    sel_y = true(size(yvec));
+
+end
+
+% Call imregdemons to calculate the displacement field D that maps from the
+% fixed to the moving image.
+img1_reg = NaN(size(img2));
+[D,I_reg] = imregdemons(img1(sel_y,sel_x),img2(sel_y,sel_x),N,...
+    'AccumulatedFieldSmoothing',afs,...
+    'PyramidLevels',pl,...
+    'DisplayWaitbar',dw);
+img1_reg(sel_y,sel_x,:) = I_reg;
+
+
+% Transform the points
+if ~isempty(points)
+    if iscell(points)
+        % Cell array with multiple point sets is provided. Transform all 
+        % points per cell.
+        points_tf = cell(size(points));
+        for i = 1 : numel(points)
+            if ~isempty(points{i})
+                points_tf{i} = get_new_location(points{i});
+            end
+        end
+    else
+        % One array with points is provided.        
+        points_tf = get_new_location(points);
+    end
+end
+
+% Sub-functions
+    function pnew = get_new_location(pts)        
+            d = zeros(size(pts));
+            d(:,1) = interp2(xvec(sel_x),yvec(sel_y),D(:,:,1),pts(:,1),pts(:,2),method); % x displacement in pixel coordinates
+            d(:,2) = interp2(xvec(sel_x),yvec(sel_y),D(:,:,2),pts(:,1),pts(:,2),method); % y displacement in pixel coordinates
+            % Update the new position vectors. The displacement field
+            % predicted by imregdemons is in pixel coordinates, so it needs
+            % to be converted back to image coordinates (flipped x/y axis,
+            % and multiplied by pixel size).
+            pnew = pts + d.*spacing([2 1]);        
+    end
+%%
+if nargout > 0
+    varargout{1} = img1_reg;
+    if nargout > 1
+        varargout{2} = points_tf;
+%         if nargout > 2
+%             varargout{3} = label1_tf;
+%         end
+    end
+end
+
+
+end
+
---
+++ old_versions/snap_to_edge.m
@@ -0,0 +1,165 @@
+function [newpoints,Gmag,Gdir] = snap_to_edge(img,points,varargin)
+%SNAP_TO_EDGE Snaps points to nearest edge of 2D image I.
+% img     : 2D image
+% points  : n x 2 set of points or a cell array with n x 2 point sets.
+% Optional inputs (as 'argument',<value> pairs)
+% spacing : pixel spacing in mm. Default: [1 1]
+% maxd    : maximum distance to move points (in mm). Default: 1
+% sigma   : standard deviation of the gaussian image filter. Default: 1
+% method  : interpolation method. Default: 'linear'.
+% direction: direction in which points can be moved: 'normal' (in direction
+%            normal to curve) or 'gradient' (orthogonal to the image gradient)
+%
+% Bart Bolsterlee
+% Neuroscience Research Australia
+% 17/04/2020
+
+%% Parse inputs
+p = inputParser;
+addRequired(p,'img')
+addRequired(p,'points')
+addParameter(p,'maxd',1,@isscalar)
+addParameter(p,'spacing',[1 1])
+addParameter(p,'sigma',1,@(x) isscalar(x) || isempty(x))
+addParameter(p,'method','linear')
+addParameter(p,'direction','gradient')
+parse(p,img,points,varargin{:});
+
+method  = p.Results.method; % interpolation method
+spacing = p.Results.spacing;
+maxd    = p.Results.maxd;
+sigma   = p.Results.sigma;
+direction = p.Results.direction;
+
+PlotFlag = false; % Set to true for diagnostics purposes only.
+%% Crop image
+% Build up x and y-vector of image.
+imdim = size(img);
+xvec = (0.5 : imdim(2)-0.5) * spacing(2);
+yvec = (0.5 : imdim(1)-0.5) * spacing(1);
+
+flag = false;
+if isnumeric(points)
+    % Convert points to cell.
+    flag = true;
+    points = {points};
+end
+
+% Crop image to location where points are. This will speed up image
+% gradient calculation.
+% Note: this will give a problem if points are near the edge of the image...
+padding = 10*maxd;
+minx = min(cellfun(@(x) min(x(:,1)),points));
+maxx = max(cellfun(@(x) max(x(:,1)),points));
+miny = min(cellfun(@(x) min(x(:,2)),points));
+maxy = max(cellfun(@(x) max(x(:,2)),points));
+sel_x = xvec > (minx - padding) & xvec < (maxx + padding);
+sel_y = yvec > (miny - padding) & yvec < (maxy + padding);
+
+
+%% Calculate gradient direction and magnitude
+if isempty(sigma)
+    Ifilt = img(sel_y,sel_x);
+else
+    Ifilt = imgaussfilt(img(sel_y,sel_x),sigma);
+end    
+[Gmag,Gdir] = imgradient(Ifilt);
+
+%%
+
+n = 1001; % sample n points along the direction vector
+d = linspace(-maxd,maxd,n)';
+newpoints = cell(size(points));
+if PlotFlag == true
+    figure('Color','w')
+    subplot(1,2,1)
+    xlabel('Distance from original location (mm)')
+    ylabel('Gradient magnitude')
+end
+for i = 1 : numel(points)
+    % Calculate edge vectors in the gradient direction.
+    N = NaN(size(points{i}));
+    switch direction
+        case 'gradient'
+            N(:,1) = interp2(xvec(sel_x),yvec(sel_y),-cosd(Gdir),points{i}(:,1),points{i}(:,2),method);
+            N(:,2) = interp2(xvec(sel_x),yvec(sel_y), sind(Gdir),points{i}(:,1),points{i}(:,2),method);
+        case 'normal'
+            [~,~,~,~,N] = fit_spline(points{i});
+    end
+    
+    Nx = d*N(:,1)' + ones(n,1)*points{i}(:,1)';
+    Ny = d*N(:,2)' + ones(n,1)*points{i}(:,2)';
+    
+    % Interpolate image at points along vector
+    G_n = interp2(xvec(sel_x),yvec(sel_y),Gmag,Nx,Ny,method);
+    
+    % Find all peaks of curve and use the one closest to 0 (i.e. closest to
+    % the position before snapping. This prevents snapping to the other
+    % side of thin objects.
+    nPoints = size(G_n,2);
+    max_value = NaN(nPoints,1);
+    idx       = NaN(nPoints,1);
+    for j = 1 : nPoints
+        % Index of all maxima of the curve
+        max_idx = find(islocalmax(G_n(:,j))); 
+        if isempty(max_idx)
+            % No edge found within maximum distance. Keep the original
+            % position.
+            idx(j) = find(d==0);
+        else
+            % Get values of all maxima of the curve.
+            all_max_value = G_n(max_idx,j);
+            
+            % Exclude local maxima that are less than half as high as the
+            % global maximum. This should prevent snapping to trivial
+            % edges.
+            excl = all_max_value < 0.5*max(all_max_value);
+            max_idx(excl) = [];
+            [~,closest_idx] = min(abs(d(max_idx))); % index of closes maxima
+            idx(j) = max_idx(closest_idx);
+        end
+        max_value(j) = G_n(idx(j),j);
+
+    end
+    if PlotFlag == true
+        hold on
+        plot(d,G_n,'k-')
+        plot(d(idx),max_value,...
+            'o',...
+            'MarkerSize',8,...
+            'MarkerFaceColor','r',...
+            'MarkerEdgeColor','k')
+    end
+    
+    % Calculate new position
+    newpoints{i} = points{i} + d(idx)*[1 1] .* N;    
+end
+
+% Plot for diagnostics:
+if PlotFlag == true
+    subplot(1,2,2)
+    image(xvec(sel_x),yvec(sel_y),Gmag)
+    colormap gray
+    hold on
+    for i = 1 : length(points)
+        h1 = plot(points{i}(:,1),points{i}(:,2),'-oy',...
+            'LineWidth',2,...
+            'MarkerFaceColor','y',...
+            'MarkerEdgeColor','none',...
+            'MarkerSize',6);
+        h2 = plot(newpoints{i}(:,1),newpoints{i}(:,2),'-or',...
+            'LineWidth',2,...
+            'MarkerFaceColor','none',...
+            'MarkerEdgeColor','r',...
+            'MarkerSize',7);
+    end
+    legend([h1 h2],{'before','after'});
+    axis equal off tight
+end
+if flag == true
+    newpoints = newpoints{1};
+end
+
+
+end
+
